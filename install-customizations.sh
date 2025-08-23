437520ce45530e26465d5cc9639a3860c5b7eef1
David Snyder <37163053+AlwaysLearningTech@users.noreply.github.com>
2025-08-23 00:07:06 -0700

    Create install-customizations.sh

---
+++ install-customizations.sh
@@ -0,0 +1,8 @@
+sudo wget https://github.com/balena-io/etcher/releases/download/v2.1.4/balena-etcher_2.1.4_amd64.deb
+apt install ./balena-etcher_2.1.4_amd64.deb
+apt install libreoffice
+
+et-mirror https://choisser.com/packet/
+et-mirror https://www.cantab.net/users/john.wiseman/Documents/
+et-mirror https://soundcardpacket.org/
+et-mirror https://tldp.org/HOWTO/AX25-HOWTO/index.html
