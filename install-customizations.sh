f06c4a0a9620805a12774762ed4629d7c8c8b54e
David Snyder <37163053+AlwaysLearningTech@users.noreply.github.com>
2025-09-19 10:21:23 -0700

    Corrections to new prefs

--- install-customizations.sh
+++ install-customizations.sh
@@ -3,19 +3,27 @@
 # Turn off on-screen keyboard
 gsettings set org.gnome.desktop.a11y.applications screen-keyboard-enabled false
 
-# Turn off large text
-gsettings set org.gnome.desktop.a11y.interface large-text false
+# Disable large text accessibility setting and display scaling to 100%
+gsettings set org.gnome.desktop.interface text-scaling-factor 1.0
+gsettings set org.gnome.desktop.interface scaling-factor 1
 
 # Turn on dark mode system-wide
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 gsettings set org.gnome.desktop.interface gtk-theme 'Yaru-dark'
 
-# Set display scaling to 100%
-gsettings set org.gnome.desktop.interface text-scaling-factor 1.0
-gsettings set org.gnome.desktop.interface scaling-factor 1
+# Store WiFi credentials
+DEFAULT_WIFI_SSID="321"
+DEFAULT_WIFI_PASSWORD="BlueLaptop3!"
+ALT_WIFI_SSID="David's iPhone"
+ALT_WIFI_PASSWORD="25HotSpot"
 
-# Connect to WiFi SSID 'David's iPhone'
-nmcli dev wifi connect "David's iPhone" password "25HotSpot"
+# Add default WiFi connection (SSID: 321)
+nmcli connection add type wifi ifname "*" con-name "$DEFAULT_WIFI_SSID" autoconnect yes ssid "$DEFAULT_WIFI_SSID"
+nmcli connection modify "$DEFAULT_WIFI_SSID" wifi-sec.key-mgmt wpa-psk wifi-sec.psk "$DEFAULT_WIFI_PASSWORD"
+
+# Add alternate WiFi connection (SSID: David's iPhone)
+nmcli connection add type wifi ifname "*" con-name "$ALT_WIFI_SSID" autoconnect no ssid "$ALT_WIFI_SSID"
+nmcli connection modify "$ALT_WIFI_SSID" wifi-sec.key-mgmt wpa-psk wifi-sec.psk "$ALT_WIFI_PASSWORD"
 
 # Install Chirp
 wget https://archive.chirpmyradio.com/chirp_next/next-20250822/chirp-20250822-py3-none-any.whl
@@ -23,11 +31,12 @@ pipx install --system-site-packages ./chirp-20250822-py3-none-any.whl
 pipx ensurepath
 cp chirp.desktop /etc/skel/.local/share/applications/chirp.desktop
 
+# Install dmrconfig, LibreOffice
+sudo apt update
+sudo apt install -y dmrconfig, libreoffice
 
-apt install libreoffice
-
-et-mirror https://choisser.com/packet/
-et-mirror https://www.cantab.net/users/john.wiseman/Documents/
-et-mirror https://soundcardpacket.org/
-et-mirror https://tldp.org/HOWTO/AX25-HOWTO/index.html
+et-mirror.sh https://choisser.com/packet/
+et-mirror.sh https://www.cantab.net/users/john.wiseman/Documents/
+et-mirror.sh https://soundcardpacket.org/
+et-mirror.sh https://tldp.org/HOWTO/AX25-HOWTO/index.html
 
